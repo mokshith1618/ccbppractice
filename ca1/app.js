905ddb294b7a4d5edbd62926e7790aeb86938c84
mokshith <83539464+mokshith1618@users.noreply.github.com>
2022-10-23 11:41:10 +0530

    Update app.js

--- ca1/app.js
+++ ca1/app.js
@@ -29,7 +29,7 @@ const invalidityConditions = (status, priority, category, dueDate, date) => {
   } else if (!["TO DO", "IN PROGRESS", "", "DONE"].includes(status)) {
     return "Invalid Todo Status";
   } else if (!["WORK", "HOME", "", "LEARNING"].includes(category)) {
-    return "Invalid Todo category";
+    return "Invalid Todo Category";
   } else if (!isValid(new Date(dueDate)) && !dueDate == "") {
     return "Invalid Due Date";
   } else if (!isValid(new Date(date)) && !date == "") {
@@ -82,20 +82,16 @@ app.get("/todos/", checkValidity, async (request, response) => {
     status = "",
     priority = "",
   } = request.query;
-  const formattedDueDate = "";
-  if (dueDate !== "") {
-    formattedDueDate = format(new Date(dueDate), "yyyy-MM-dd");
-  }
   const getTodosQuery = `
     SELECT 
         *
     FROM 
         todo
     WHERE 
-        todo LIKE '%${search_q}%' and
+       ( todo LIKE '%${search_q}%' and
         category LIKE '%${category}%' and
         status LIKE '%${status}%' and
-        priority LIKE '%${priority}%';`;
+        priority LIKE '%${priority}%');`;
   const todos = await db.all(getTodosQuery);
   response.send(todos.map((item) => formattedTodosList(item)));
 });
@@ -117,15 +113,77 @@ app.get("/agenda/", checkValidity, async (request, response) => {
   if (date !== "") {
     formattedDueDate = format(new Date(date), "yyyy-MM-dd");
   }
-  const getTodoQuery = `
+  const getTodosQuery = `
+    SELECT 
+         *
+    FROM 
+        todo
+    WHERE
+        strftime('%Y-%m-%d',due_date) LIKE '%${formattedDueDate}%';`;
+  const todos = await db.all(getTodosQuery);
+  response.send(todos.map((item) => formattedTodosList(item)));
+});
+app.post("/todos/", checkValidity, async (request, response) => {
+  const { id, todo, priority, status, category, dueDate } = request.body;
+  const formattedDueDate = format(new Date(dueDate), "yyyy-MM-dd");
+  const postTodoQuery = `
+    INSERT INTO 
+        todo(id,todo,priority,status,category,due_date)
+    VALUES 
+        (${id},'${todo}','${priority}','${status}','${category}','${formattedDueDate}');`;
+  const dbResponse = await db.run(postTodoQuery);
+  response.send("Todo Successfully Added");
+});
+app.delete("/todos/:todoId/", checkValidity, async (request, response) => {
+  const { todoId } = request.params;
+  const deleteTodoQuery = `
+    DELETE FROM todo
+    WHERE 
+        id=${todoId};`;
+  const dbResponse = await db.run(deleteTodoQuery);
+  response.send("Todo Deleted");
+});
+app.put("/todos/:todoId/", checkValidity, async (request, response) => {
+  const { todoId } = request.params;
+  `sptcd`;
+  let col;
+  if (request.body.status !== undefined) {
+    col = "Status";
+  } else if (request.body.priority !== undefined) {
+    col = "Priority";
+  } else if (request.body.todo !== undefined) {
+    col = "Todo";
+  } else if (request.body.category !== undefined) {
+    col = "Category";
+  } else if (request.body.dueDate !== undefined) {
+    col = "Due Date";
+  }
+  const selectTodoQuery = `
     SELECT 
         *
     FROM 
         todo
     WHERE 
-        due_date = strftime("%Y-%m-%d",'${formattedDueDate}');`;
-  const todo = await db.get(getTodoQuery);
-  console.log(formattedDueDate);
-  response.send(todo);
+       id=${todoId} ;`;
+  const previousTodo = await db.get(selectTodoQuery);
+  const {
+    todo = previousTodo.todo,
+    category = previousTodo.category,
+    priority = previousTodo.priority,
+    status = previousTodo.status,
+    dueDate = previousTodo.due_date,
+  } = request.body;
+  const formattedDueDate = format(new Date(dueDate), "yyyy-MM-dd");
+  const putTodoQuery = `
+    UPDATE todo
+    SET 
+        id=${todoId},
+        todo='${todo}',
+        category='${category}',
+        priority='${priority}',
+        status='${status}',
+        due_date='${formattedDueDate}';`;
+  const dbResponse = await db.run(putTodoQuery);
+  response.send(`${col} Updated`);
 });
 module.exports = app;
